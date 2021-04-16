f434411a8d6755b54562b6af878209fc6bfb71c6
Tyler Hance <tyler.hance@gmail.com>
2021-04-15 22:13:56 -0700

    Creating variables in JS and building outline of  buttons in html.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Variables
+var startButton = document.querySelector(".start-button");
+var timer;
+var timerCountEl = document.querySelector(".timer-count");
+var questions;
+var userInput;
+var answerCorrect;
+var score;
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+
+var questions = [""]
