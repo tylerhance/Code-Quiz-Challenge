440de2a01f6d8b806a17534d249322525f71adb0
Tyler Hance <tyler.hance@gmail.com>
2021-04-17 18:13:35 -0700

    Building out questions and answers in an array.

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ var score;
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
 
-var questions = [""]
+var questions = [
+    {
+        question: "What is an array?", 
+        choices: ["An ordered list of values.", "An action performed on objects.", "A behaviour declaration", "The properties of all HTML elements."], 
+        answer: "An ordered list of values.",   
+    },
+    {
+        question: "JavaScript is primarily used in the browser, enabling developers to manipulate webpage content through the ____?", 
+        choices: ["Terminal", "Code Editor", "DOM", "Browser"], 
+        answer: "DOM",   
+    }
+] 
+console.log(questions[0].question);
+
